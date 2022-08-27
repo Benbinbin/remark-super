a41c296e1b09c7b4f2b4bc887fe8279938b2818f
Ben <3126319718@qq.com>
2022-08-27 12:37:53 +0800

    fix  bug.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import { visit } from 'unist-util-visit'
 
 // the regular expression to match the subscript format in markdown, like ~word~
-const REGEX = /_[^_]+_/g
+const REGEX = /\^[^^]+\^/g
 
 // refer to https://github.com/rhysd/remark-emoji
-export default function remarkSub() {
+export default function remarkSuper() {
   function transformer(ast) {
     // refer to https://www.ryanfiller.com/blog/remark-and-rehype-plugins
     visit(ast, 'text', (node) => {
       if (REGEX.test(node.value)) {
         const newValue = node.value.replace(REGEX, (match) => {
-          return '<sub>' + match.substring(1, match.length - 1) + '</sub>'
+          return '<sup>' + match.substring(1, match.length - 1) + '</sup>'
         })
-
         Object.assign(node, { type: 'html', value: newValue })
       }
     })
